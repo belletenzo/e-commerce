3a5d82b566fd80ce1adf0fe181d5c83e1a2f7182
Enzo <enzo.bellet@le-campus-numerique.fr>
2022-08-03 11:19:57 +0200

    Choix de la quantité

--- test/multidimensional-catalog.php
+++ test/multidimensional-catalog.php
@@ -5,39 +5,72 @@ $products = [
         "price" => 150000,
         "weight" => 2000,
         "discount" => 10,
-        "picture_url" => "https://images.unsplash.com/photo-1542729716-6d1890d980ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aW5mb3JtYXRpcXVlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        "picture_url" => "./images/pcgamer.avif",
     ],
     "Playstation 5" => [
         "name" => "Playstation 5",
         "price" => 50000,
         "weight" => 700,
         "discount" => 5,
-        "picture_url" => "https://images.unsplash.com/photo-1607853202273-797f1c22a38e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80",
+        "picture_url" => "./images/ps5.avif",
     ],
     "Xbox" => [
         "name" => "Xbox series X",
         "price" => 50000,
         "weight" => 700,
         "discount" => 25,
-        "picture_url" => "https://images.unsplash.com/photo-1621259182978-fbf93132d53d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80",
+        "picture_url" => "./images/xboxX.avif",
+    ],
+    "Switch" => [
+        "name" => "Nintendo Switch",
+        "price" => 30000,
+        "weight" => 200,
+        "discount" => 5,
+        "picture_url" => "./images/switch.avif",
     ],
 ];
 require "my-functions.php";
+include 'head.php';
 ?>
 
 
-<table>
-    <?php foreach ($products as  $key => $product ){ ?>
-    <tr>
-        <td>Nom : <?= $product["name"] . "\n"; ?></td>
-        <td>Prix : <?= formatPrice($product["price"]) . "\n"; ?></td>
-        <td>Prix HT : <?= formatPrice(priceExcludingVAT($product["price"])) . "\n"; ?></td>
-        <td>Poids : <?= $product["weight"] . "\n";?>g</td>
-        <td>Promo de : <?= $product["discount"]?>% = <?= formatPrice(discountedPrice($product["price"] ,$product["discount"])) . "\n";?></td>
-        <td><img src="<?= $product["picture_url"]?>" alt="" width="200"></td>
-    </tr>
-    <?php } ?>
+<div class="container-fluid w-75 mt-4">
+    <div class="row">
 
-</table>
+        <?php foreach ($products as $key => $product) { ?>
+
+
+            <div class="col-lg-3 col-md-6 col-sm-6">
+                <div class="card mb-4 shadow-sm border border-3 border-info">
+
+                    <div class="card-body ">
+                        <p>Nom : <?= $product["name"] . "\n"; ?></p>
+
+                        <p>Prix : <?= formatPrice($product["price"]) . "\n"; ?></p>
+
+                        <p>Prix HT : <?= formatPrice(priceExcludingVAT($product["price"])) . "\n"; ?></p>
+
+                        <p>Poids : <?= $product["weight"] . "\n"; ?>g</p>
+
+                        <p>Promo de : <?= $product["discount"] ?>%
+                            = <?= formatPrice(discountedPrice($product["price"], $product["discount"])) . "\n"; ?></p>
+
+                        <p>
+                        <form method="post">
+                            <label for="number">Quantité a acheter : </label>
+                            <input type="number" name="quantité" id="quantité" required min="1">
+                            <input type="submit" value="Acheter">
+                        </form>
+                        </p>
+                    </div>
+
+                    <p><img src="<?= $product["picture_url"] ?>" alt="" class="w-100 img-fluid"></p>
+                </div>
+            </div>
+
+        <?php } ?>
+
+    </div>
+</div>
 
 
